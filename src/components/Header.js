fd76cdadaaa9a015e54e7d90af8bd69e881d3ff8
Khushbakht Khan <103233325+khushbakhtkhan1@users.noreply.github.com>
2024-02-21 17:36:48 +0500

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,30 @@
 import React from "react";
+import { Typography,Box } from "@mui/material";
+import Link from "next/link";
+import Image from "next/image"
+
+const imagePath = "/images/header.jpg";
 
 const Header = () => {
-  const imagePath = "/images/header.jpg";
+
   return (
-    <div style={{ marginBottom: 10 }}>
-      <h1 className="h1">
-        <img src={imagePath} alt="Header image" width="100" height="100" />
-        React Hook Form
-      </h1>
-      <p style={{ fontSize: 14, lineHeight: 1.3, marginBottom: 0 }}>
-        Performant, flexible and extensible forms with easy-to-use validation.
-      </p>
-      <a
-        className={"preact"}
-        target="_blank"
-        href="https://codesandbox.io/s/preact-2zsw6?file=/src/index.js"
-        style={{
-          fontSize: 10,
-          height: 20,
-        }}
-      >
-        🐰 Show me the Preact version
-      </a>
-    </div>
+
+    <Box >
+      <Image src={imagePath} alt="Header image" width={100} height={100}/>
+      <Box>
+        <Typography variant="h2">React Hook Form</Typography>
+        <Typography variant="h6">
+          Performant, flexible and extensible forms with easy-to-use validation.
+        </Typography>
+        <Link
+          href="https://codesandbox.io/s/preact-2zsw6?file=/src/index.js"
+        >
+          <Typography >
+            🐰 Show me the Preact version
+          </Typography>
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
